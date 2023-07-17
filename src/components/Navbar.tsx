bffb5edf78eeef246cf717d8edd9184462cd68f8
cgathua <114153640+cgathua@users.noreply.github.com>
2023-07-17 08:49:06 -0400

    create: mobile nav

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,9 @@
 import { Fragment } from "react";
+import { useState } from "react";
 
-Fragment
+
 function Navbar()  {
-   
+    const[isNavShowing, setisNavshowing] = useState(false);
     let items = [
         { id: 1, link: '#', title: ''},
         { id: 2, link: '#home', title: 'Home'},
@@ -10,6 +11,15 @@ function Navbar()  {
         { id: 4, link: '#faqs', title: 'FAQ'},
         { id: 5, link: '#reviews', title: 'Reviews'},
         { id: 6, link: '#contact', title: 'Contact'}
+    ]
+
+    let ulLists = [
+        { id: 7, link: '#', title: ''},
+        { id: 8, link: '#home', title: 'Home'},
+        { id: 9, link: '#services', title: 'Services'},
+        { id: 10, link: '#faqs', title: 'FAQ'},
+        { id: 11, link: '#reviews', title: 'Reviews'},
+        { id: 12, link: '#contact', title: 'Contact'}
     ]
 
     return (
@@ -21,7 +31,10 @@ function Navbar()  {
                     <ul className="nav_links">
                         {items.map(item => <li key={item.id}><a href={item.link}>{item.title}</a></li>)}   
                     </ul>
-                    <button className="menu-btn" aria-label="menu" aria-haspopup="true" aria-expanded="false"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#ACACB2" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-menu"><line x1="3" y1="12" x2="21" y2="12"></line><line x1="3" y1="6" x2="21" y2="6"></line><line x1="3" y1="18" x2="21" y2="18"></line></svg></button>
+                    <ul className={`navbar2 ${isNavShowing ? 'show_nav' : 'hide_nav'}`}>
+                        {ulLists.map(elem => <li key={elem.id}><a href={elem.link}>{elem.title}</a></li>)}
+                    </ul>
+                    <button className="menu-btn" onClick={() => setisNavshowing(!isNavShowing)} aria-label="menu" aria-haspopup="true" aria-expanded="false"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#ACACB2" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-menu"><line x1="3" y1="12" x2="21" y2="12"></line><line x1="3" y1="6" x2="21" y2="6"></line><line x1="3" y1="18" x2="21" y2="18"></line></svg></button>
                 </nav>
             </header>
         </Fragment>
